01d9bbe10a8354488a82d6831371cc0027b920ff
akihito.nakano <sora.akatsuki@gmail.com>
2019-10-06 18:09:02 +0900

    Read configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+#[macro_use]
+extern crate serde_derive;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, PBFT!");
+    let c = read_config();
+    println!("{:?}", c);
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    nodes: Vec<Port>,
+    primary: Port,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Port {
+    port: u64,
+}
+
+fn read_config() -> Result<Config, String> {
+    let content = match std::fs::read_to_string("network.json") {
+        Ok(c) => c,
+        Err(e) => return Err(format!("Failed to read network.json: {}", e))
+    };
+
+    println!("{}", content);
+    match serde_json::from_str(&content) {
+        Ok(c) => Ok(c),
+        Err(e) => Err(format!("Failed to deserialize network.json: {}", e))
+    }
 }
