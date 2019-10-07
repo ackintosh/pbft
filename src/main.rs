6136c747de804fbb52acc51fb932999e9e8d10d8
akihito.nakano <sora.akatsuki@gmail.com>
2019-10-08 07:52:58 +0900

    Parse CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@ mod config;
 
 fn main() {
     println!("Hello, PBFT!");
+
+    let args: Vec<String> = std::env::args().collect();
+    println!("Command line args: {:?}", args);
+
+    if args.len() != 2 {
+        println!("Usage: $ pbft {{port}}");
+        std::process::exit(1);
+    }
+
+    let port: u64 = args
+        .get(1).expect("Failed to get port number via CLI arguments")
+        .parse().expect("Failed to parse the argument as port number");
+    println!("port: {}", port);
+
     let config = match config::read_config() {
         Ok(c) => {
             println!("{:?}", c);
