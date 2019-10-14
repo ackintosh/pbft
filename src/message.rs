33fbc34b767b4e553b14ca5ea4e7fe5f9dd16dc3
akihito.nakano <sora.akatsuki@gmail.com>
2019-10-14 17:33:57 +0900

    Add PrePrepare message

--- src/message.rs
+++ src/message.rs
@@ -12,3 +12,22 @@ impl Request {
         serde_json::from_str(s).unwrap()
     }
 }
+
+pub struct PrePrepare {
+    // view indicates the view in which the message is being sent
+    view: u64,
+    // sequence number for pre-prepare messages
+    n: u64,
+    // client message's digest
+    digest: String,
+}
+
+pub struct PrePrepareSequence {
+    value: u64,
+}
+
+impl PrePrepareSequence {
+    pub fn new() -> Self {
+        Self { value: 1 }
+    }
+}
